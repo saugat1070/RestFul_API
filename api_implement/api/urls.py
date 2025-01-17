cac476ee23a7ab25b42ae38fdcf22c8edd394dc4
saugat1070 <saugatgiri1070@gmail.com>
2025-01-18 00:57:56 +0545

    append api_root path which show url of movie and platform

--- api_implement/api/urls.py
+++ api_implement/api/urls.py
@@ -7,11 +7,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.api_root,name='api_root'),
     path('list/',views.movie_list.as_view(),name='movie_list'),
     path('list/<int:pk>',views.movie_details.as_view(),name='movie_details'),
-    path('stream/',views.stream_list.as_view(),name='stream'),
-    path('stream/<int:pk>',views.stream_details.as_view(),name='stream'),
+    path('stream/',views.stream_list.as_view(),name='stream_list'),
+    path('stream/<int:pk>',views.stream_details.as_view(),name='stream_details'),
     
     
 ]
